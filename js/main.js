9358c7783edbfbe7e07bf8c64fb6e784272f600b
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-05-31 19:19:00 +0500

    select2

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ $(".header_mobile .burgir").click(function() {
 $(".footer__block h5").click(function() {
     $(this).toggleClass('active');
 });
+
+if(document.querySelector(".services__table .top .left")) {
+    $(".services__table .top .left").click(function() {
+        $(this).toggleClass('active');
+        $(".services__aside").toggleClass('active');
+    });
+};
+if(document.querySelector(".jobs__select")) {
+    $(".jobs__select").click(function() {
+        $(this).toggleClass('active');
+        $(".services__aside").toggleClass('active');
+    });
+};
