b24225dceffd808066d1e1fd4ebb45e55396b840
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-06-03 01:09:15 +0500

    modal

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
+$(".header__call").click(function() {
+    $(".modal").addClass('active');
+});
+$(".modal__close").click(function() {
+    $(".modal").removeClass('active');
+});
+
 $(".jobs__block .btn").click(function() {
     $(".jobs__modal").addClass('active');
 });
 $(".jobs__modal_close").click(function() {
     $(".jobs__modal").removeClass('active');
 });
+
 $(".header_mobile .burgir").click(function() {
     $(".header").toggleClass('active');
     $(this).toggleClass('active');
 });
+
 $(".footer__block h5").click(function() {
     $(this).toggleClass('active');
 });
@@ -18,6 +27,7 @@ if(document.querySelector(".services__table .top .left")) {
         $(".services__aside").toggleClass('active');
     });
 };
+
 if(document.querySelector(".jobs__select")) {
     $(".jobs__select").click(function() {
         $(this).toggleClass('active');
